618e5e88cfdd368886890152d5576d7c88d3faff
Kuredew <burrohmanhabib35@gmail.com>
2026-08-01 17:28:50 +0700

    fix: again

--- src/lib/util/mongoose.ts
+++ src/lib/util/mongoose.ts
@@ -1,18 +1,28 @@
-import { type Types } from 'mongoose';
+import type { Types } from 'mongoose';
 
 interface VercelBinaryId {
 	buffer: Uint8Array | ArrayBuffer;
-	toString(): string;
 }
 
 export type AcceptableIdInput = string | Types.ObjectId | VercelBinaryId | null | undefined;
+export type StrictMongooseIdOutput = string | Types.ObjectId | null | undefined;
 
-export function castServerlessId(v: AcceptableIdInput): AcceptableIdInput {
+export function castServerlessId(v: AcceptableIdInput): StrictMongooseIdOutput {
 	if (!v) {
-		return v;
+		return v as null | undefined;
 	}
+
 	if (typeof v === 'object' && 'buffer' in v) {
-		return v.toString();
+		const rawBuffer = v.buffer;
+
+		if (rawBuffer instanceof Uint8Array) {
+			return Buffer.from(rawBuffer).toString('hex');
+		}
+
+		if (rawBuffer instanceof ArrayBuffer) {
+			return Buffer.from(new Uint8Array(rawBuffer)).toString('hex');
+		}
 	}
-	return v;
+
+	return v as string | Types.ObjectId;
 }
